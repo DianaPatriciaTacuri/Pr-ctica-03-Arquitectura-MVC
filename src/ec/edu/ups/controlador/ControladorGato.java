b652735dd459a70d0d24e83f7371a1b87f03c894
one <one@usuario>
2019-04-23 19:08:00 -0500

    creacion de la clase Controlador usando la interface Set y la clase HashSet.

--- src/ec/edu/ups/controlador/ControladorGato.java
+++ src/ec/edu/ups/controlador/ControladorGato.java
@@ -5,10 +5,49 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.clases.Gato;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author one
  */
 public class ControladorGato {
+    private Set<Gato>lista;
+    private int codigo;
+    
+    public ControladorGato(){
+        lista = new HashSet<>();
+        codigo=0;
+        
+    }
+    public void crear(Gato objeto){
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.add(objeto);
+    }
+    public Gato read(int codigo){
+        for(Gato gato: lista){
+            if(gato.getCodigo()== codigo){
+                return gato;
+            }
+        }
+        return null;
+    }
+    public void update(Gato objeto){
+        if(lista.contains(objeto)){
+            lista.remove(objeto);
+            lista.add(objeto);
+        }
+    }
+    public void delete(int codigo){
+        for(Gato gato:lista){
+            if(gato.getCodigo()==codigo){
+                lista.remove(gato);
+                break;
+            }
+        }
+    }
     
 }
