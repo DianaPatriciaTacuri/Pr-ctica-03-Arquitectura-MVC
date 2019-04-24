2696d8cb3f2b80c0d09394cef39357dc556d3a65
one <one@usuario>
2019-04-23 20:34:04 -0500

    creacion de la clase Controlador usando la interface Map y la clase HashMap

--- src/ec/edu/ups/controlador/ControladorVaca.java
+++ src/ec/edu/ups/controlador/ControladorVaca.java
@@ -5,10 +5,54 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.clases.Perro;
+import ec.edu.ups.clases.Vaca;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  *
  * @author one
  */
 public class ControladorVaca {
+     private Map<Integer,Vaca> mapa;
+    private int codigo;
+    
+    public ControladorVaca(){
+        mapa = new HashMap<>();
+        codigo = 0;
+    }
+    public void crear(Vaca vaca){
+        codigo++;
+        vaca.setCodigo(codigo);
+        mapa.put(codigo,vaca);
+    }
+    public  Vaca read(int codigo){
+        for (Entry<Integer,Vaca> vaca :mapa.entrySet() ) {
+            if(vaca.getKey()==codigo){
+                System.out.println(vaca);
+                
+            }
+        }
+        return null;
+    }
+    public void update(Vaca vaca,int codigo){
+        if(mapa.containsKey(codigo)){
+            mapa.remove(codigo,vaca);
+            mapa.put(codigo, vaca);
+        }
+    }
+    public void dellete(int codigo){
+        for (Entry<Integer,Vaca> vaca:mapa.entrySet()) {
+            if(vaca.getKey()== codigo){
+                mapa.remove(codigo);
+                break;
+            }
+        }
+    }
+    public void leer(){
+        
+    }
     
 }
