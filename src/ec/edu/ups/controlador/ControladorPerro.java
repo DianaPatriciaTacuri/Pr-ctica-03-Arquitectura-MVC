e6df04f683b06780cda67bf8163a1a5c2c16489e
one <one@usuario>
2019-04-23 19:17:51 -0500

    creacion de la clase Controlador usando la interface SortedSet y la clase TreeSet

--- src/ec/edu/ups/controlador/ControladorPerro.java
+++ src/ec/edu/ups/controlador/ControladorPerro.java
@@ -5,10 +5,51 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.clases.Perro;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  *
  * @author one
  */
 public class ControladorPerro {
+    private SortedSet<Perro> lista;
+    private int codigo;
+    public  ControladorPerro(){
+        lista= new TreeSet<>();
+        codigo=0;
+    }
+    public void crear(Perro objeto){
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.add(objeto);
+    }
+    public  Perro read(int codigo){
+        for (Perro perro : lista) {
+            if(perro.getCodigo()==codigo){
+                return perro;
+            }
+        }
+        return null;
+    }
+    public void update(Perro objeto){
+        if(lista.contains(objeto)){
+            lista.remove(objeto);
+            lista.add(objeto);
+        }
+    }
+    public void dellete(int codigo){
+        for (Perro perro : lista) {
+            if(perro.getCodigo()== codigo){
+                lista.remove(perro);
+                break;
+            }
+        }
+    }
+    public void imprimir(Perro objeto){
+        
+    }
+    
     
 }
