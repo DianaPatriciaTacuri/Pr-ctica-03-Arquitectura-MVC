e534e38cd951a48dd617febd24cfca9fd77c883f
one <one@usuario>
2019-04-23 19:03:23 -0500

    creacion de la clase Controlador usando la interface List y la clase ArrayList.

--- src/ec/edu/ups/controlador/ControladorCaballo.java
+++ src/ec/edu/ups/controlador/ControladorCaballo.java
@@ -5,10 +5,56 @@
  */
 package ec.edu.ups.controlador;
 
+import ec.edu.ups.clases.Caballo;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author one
  */
 public class ControladorCaballo {
+     private List<Caballo> lista;
+
+    int contador=0;
+
+    public ControladorCaballo() {
+        lista=new ArrayList<>();
+    
+    }
+    
+    public void guardar(Caballo objeto){
+        lista.add(objeto);
+    }
+    public Caballo read(int codigo){
+        for (Caballo caballo : lista) {
+            if (caballo.getCodigo()==codigo) {
+                return caballo; 
+            }
+        }
+        return null;
+    } 
+
+public void update(Caballo objeto){
+    for (int i = 0; 1 < lista.size(); i++) {
+        Caballo elemento= lista.get(i);
+        if(elemento.equals(objeto)){
+            lista.set(i, objeto);
+            break;
+        }
+        
+    }
+
+}
+public void delete (int codigo){
+        for (int i = 0; i < lista.size(); i++) {
+            Caballo elemento= lista.get(i);
+             if(elemento.getCodigo()==codigo){
+            lista.remove(i);
+            break;
+        }
+            
+        }
+    }
     
 }
